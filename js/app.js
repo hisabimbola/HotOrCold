0c0d897190d0dc4b5d2554dfcfcf9bf876ee8b3e
Abimbola Idowu <abimbola.idowu@andela.co>
2014-11-24 10:39:59 +0100

    Seperate my output from my app, by creating a method for it, that sends the feedback to the user

--- js/app.js
+++ js/app.js
@@ -11,23 +11,28 @@ var game = {
   computerGuess: 0,
   previousGuess: 0,
 
+  //initialises the GUess Game;
   init: function () {
     this.guessGame();
     this.newGameListener();
     this.submitGuessListener();
   },
+
+  showUser: function (message) {
+    this.feedback.innerHTML = message;
+  },
+
   //generates random number from 0 to 100
   guessGame: function () {
     this.computerGuess = Math.floor(Math.random() * 100);
-    this.feedback.innerHTML = "Please Enter a Guess(Number) between 0 and 100";
-    console.log(this.computerGuess);
+    this.showUser("Please Enter a Guess(Number) between 0 and 100");
     userGuess.value = "";
   },
 
   //compares user guess against, computer guess
   compareGuess: function (userGuessValue) {
     if (userGuessValue == this.computerGuess) {
-      this.feedback.innerHTML = "Your guess is correct!";
+      this.showUser("Your guess is correct!");
     } else {
       this.testGuess(this.computerGuess, userGuessValue, this.previousGuess);
     }
@@ -45,18 +50,18 @@ var game = {
     var compUserDiff = Math.abs(computerGuess - userGuess);
     var compPrevDiff = Math.abs(computerGuess - previousGuess);
     if (compUserDiff < compPrevDiff) {
-      this.feedback.innerHTML = "Getting Hotter!";
+      this.showUser("You are Getting Hotter!");
     } else if (compUserDiff > compPrevDiff) {
-      this.feedback.innerHTML = "Getting Colder!";
+      this.showUser("Getting Colder!");
     } else if (compUserDiff == compPrevDiff) {
-      this.feedback.innerHTML = "Neither Colder nor Hotter!";
+      this.showUser("Neither Colder nor Hotter!");
     }
   },
   //checks if user input is valid
   checkGuess: function() {
     var userGuessValue;
     if (/[^0-9]/g.test(this.userGuess.value) || userGuessValue > 100) {
-      this.feedback.innerHTML = "Please Enter a number between 0 and 100 only";
+      this.showUser("Please Enter a number between 0 and 100 only");
     } else {
       userGuessValue = +this.userGuess.value //converts user guess to a number and stores in a new variable
       this.compareGuess(userGuessValue);
