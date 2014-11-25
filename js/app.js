2a0703948259fecc4b4b306700aa4cfba8d57b58
Abimbola Idowu <abimbola.idowu@andela.co>
2014-11-25 09:19:02 +0100

    Styled the app, and fixed bugs, app functioning well now

--- js/app.js
+++ js/app.js
@@ -17,21 +17,18 @@ var game = {
     this.newGameListener();
     this.submitGuessListener();
   },
-
   showUser: function (message) {
     this.feedback.innerHTML = message;
   },
-
   //generates random number from 0 to 100
   guessGame: function () {
     this.computerGuess = Math.floor(Math.random() * 100);
     this.showUser("Please Enter a Guess(Number) between 0 and 100");
     userGuess.value = "";
   },
-
   //compares user guess against, computer guess
   compareGuess: function (userGuessValue) {
-    if (userGuessValue == this.computerGuess) {
+    if (userGuessValue === this.computerGuess) {
       this.showUser("Your guess is correct!");
     } else {
       this.testGuess(this.computerGuess, userGuessValue, this.previousGuess);
@@ -42,33 +39,30 @@ var game = {
     }
     this.userGuess.value = "";
   },
-
   //gives feedback base on the user previous input
   testGuess: function(computerGuess, userGuess, previousGuess) {
     //creates variable to reduce repetition
-    console.log(computerGuess, userGuess, previousGuess)
     var compUserDiff = Math.abs(computerGuess - userGuess);
     var compPrevDiff = Math.abs(computerGuess - previousGuess);
     if (compUserDiff < compPrevDiff) {
       this.showUser("You are Getting Hotter!");
     } else if (compUserDiff > compPrevDiff) {
       this.showUser("Getting Colder!");
-    } else if (compUserDiff == compPrevDiff) {
+    } else if (compUserDiff === compPrevDiff) {
       this.showUser("Neither Colder nor Hotter!");
     }
   },
   //checks if user input is valid
   checkGuess: function() {
     var userGuessValue;
-    if (/[^0-9]/g.test(this.userGuess.value) || userGuessValue > 100) {
+    if (/[^0-9]/g.test(this.userGuess.value) || userGuess.value > 100) {
       this.showUser("Please Enter a number between 0 and 100 only");
     } else {
-      userGuessValue = +this.userGuess.value //converts user guess to a number and stores in a new variable
+      userGuessValue = +this.userGuess.value; //converts user guess to a number and stores in a new variable
       this.compareGuess(userGuessValue);
     }
     userGuess.value = "";
   },
-
   //EventListeners
   //when New game button is press, guessgame function is called
   newGameListener: function () {
@@ -78,12 +72,16 @@ var game = {
   }, 
   //when submit button is pressed, checkguess is called
   submitGuessListener: function () {
+    this.userGuess.addEventListener('keypress', function(e) {
+      if (e.which == 13) {
+      game.checkGuess();
+      }
+    });
     this.submitGuess.addEventListener("click", function () {
       game.checkGuess();
     });
-  } 
+  }
 
-}
-
+};
 //calls guessGame, when windows loads
 game.init();
